8d49c449e40d73a4cc387cd4981ed87e15a3bb6b
Tommy Ren <tianze823242895@gmail.com>
2024-05-30 02:14:35 -0700

    Code format and add more items in database.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-function backgroundColor() {
+function background() {
     if (window.location.pathname === '/setting') {
         document.body.style.background = '#D9D9D9';
     }
 }
-backgroundColor();
+background();
